692eb29fbf62e1a9df123399a1e9c04ee4120ac1
Denys Kovach <48496114+Lycuferchuk@users.noreply.github.com>
2025-12-05 01:31:33 +0200

    NGRX-5 Added list layout, and product card

--- src/app/features/product-list/product-list.ts
+++ src/app/features/product-list/product-list.ts
@@ -3,10 +3,12 @@ import { PipHeader } from '../../shared/components/pip-header/pip-header';
 import { PipNavigationBar } from '../../shared/components/pip-navigation-bar/pip-navigation-bar';
 import { Product } from '../../core/models/product.model';
 import { DataService } from '../../core/services/data.service';
+import { ProductCardComponent } from '../../shared/components/product-card/product-card';
+import { Filter } from './filter/filter';
 
 @Component({
   selector: 'app-product-list',
-  imports: [PipHeader, PipNavigationBar],
+  imports: [PipHeader, PipNavigationBar, Filter, ProductCardComponent],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
--- src/app/shared/components/product-card/product-card.ts
+++ src/app/shared/components/product-card/product-card.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardImage,
+  MatCardSubtitle,
+  MatCardTitle,
+} from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { Product } from '../../../core/models/product.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [
+    MatCardHeader,
+    MatCard,
+    MatCardContent,
+    MatCardActions,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatIcon,
+    MatButton,
+    MatIconButton,
+    MatCardImage,
+    CurrencyPipe,
+  ],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss',
 })
-export class ProductCard {}
+export class ProductCardComponent {
+  @Input() product!: Product;
+
+  get stars(): number[] {
+    return Array(Math.round(this.product.rating ?? 0)).fill(0);
+  }
+
+  public addToCart(product: Product): void {
+    if (product.stock > 0) {
+      console.log('Додати в кошик:', product);
+    } else {
+      console.warn('Товар відсутній на складі');
+    }
+  }
+}
