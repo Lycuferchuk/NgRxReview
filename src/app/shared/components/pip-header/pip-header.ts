0f329cabd9957f77ac7c848179d69666134f32f2
Denys Kovach <48496114+Lycuferchuk@users.noreply.github.com>
2025-12-07 12:30:24 +0200

    NGRX-6 finish header

--- src/app/shared/components/pip-header/pip-header.ts
+++ src/app/shared/components/pip-header/pip-header.ts
@@ -3,12 +3,12 @@ import { Router } from '@angular/router';
 import { MatIconButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
-import { MatFormField } from '@angular/material/form-field';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-pip-header',
-  imports: [MatIconModule, FormsModule, MatIconButton, MatFormField, MatInput],
+  imports: [MatIconModule, FormsModule, MatIconButton, MatFormField, MatInput, MatLabel],
   templateUrl: './pip-header.html',
   styleUrl: './pip-header.scss',
 })
